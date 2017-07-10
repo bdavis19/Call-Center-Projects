760925992450874150ed18de13bed4188cf65094
Barry Davis <bdavis19@gmail.com>
2017-07-10 14:13:51 -0400

    Added the lubridate library and data.table library. This is to read large data sets quicker. I also changed the timestamps in the tables to date-time format and made sure the records in the two printed data sets are of the right type (numeric instead of chr).

--- Clean Up.R
+++ Clean Up.R
@@ -1,13 +1,15 @@
 # Environment
 library(tidyverse)
+library(lubridate)
+library(data.table)
 
 # Load Data
 # PREFIX: m - master list
-mapps <- read.csv("applications_sanitized.csv", TRUE, ",")
-mskills <- read.csv("skillsets_sanitized.csv", TRUE, ",")
-mcdns <- read.csv("cdns_sanitized.csv", TRUE, ",")
-apps <- read.csv("aggint_application.csv", TRUE, ",")
-skills <- read.csv("aggint_skillset.csv", TRUE, ",")
+mapps <- fread("applications_sanitized.csv")
+mskills <- fread("skillsets_sanitized.csv")
+mcdns <- fread("cdns_sanitized.csv")
+apps <- fread("aggint_application.csv")
+skills <- fread("aggint_skillset.csv")
 
 # Missing Values (the data was clean to begin with)
 grep("^$", mapps)
@@ -22,8 +24,12 @@ sum(is.na(skills))
 grep("^$", skills)
 
 # We need to separate the timestamp fields so that they are in a date and time field
-apps <- apps %>% separate(statTimestamp, c("Date", "Timestamp"), sep = "T")
-skills <- skills %>% separate(statTimestamp, c("Date", "Timestamp"), sep = "T")
+apps$statTimestamp <- ymd_hms(apps$statTimestamp)
+skills$statTimestamp <- ymd_hms(skills$statTimestamp)
+
+# We'll need to change the chr variables to numerics
+apps[ , 2:25 :=lapply(.SD, as.numeric), .SDcols = 2:25]
+skills[ , 2:26 :=lapply(.SD, as.numeric), .SDcols = 2:26]
 
 # Create new files for those that needed clean up.
 write.table(apps, "aggint_application_refined.csv", row.names = FALSE)
