9483f09d28fe8a1f898748e34b878832e1b84fbf
Barry Davis <bdavis19@gmail.com>
2017-10-12 16:35:55 -0400

    Added a section at the bottom for Logistic Regression.

--- Clean Up.R
+++ Clean Up.R
@@ -236,9 +236,45 @@ service_level <- 1 - erlangC * exp(exponent)
 
 
 #########################################################################################################
-# Linear Regression
-#     I don't believe this offers any valuable insight, and that there are data points I do not have
-#     access to in order to have this be valuable. Or perhaps, linear regression isn't the way to go.
+# Logistic Regression
+#     This is an attempt predict whether the call center will have a high or low call volume day.
 #########################################################################################################
-calls1 <- lm(CallsOffered ~ ApplicationID + lubridate::wday(statTimestamp) + lubridate::hour(statTimestamp) + lubridate::mday(statTimestamp), apps_primary)
-summary(calls1)
+# Check missing values
+sapply(apps_primary, function(x) sum(is.na(x)))
+sapply(apps_primary, function(x) length(unique(x)))
+library(Amelia)
+missmap(apps_primary, main="Missing values vs observed")
+
+# Get data into df by day
+apps_primary_days <- apps_primary
+apps_primary_days <- apps_primary_days %>% mutate(Date=as.Date(statTimestamp), statTimestamp = NULL, ApplicationID=NULL, ApplicationName=NULL)
+apps_primary_days <- aggregate(. ~ Date, apps_primary_days, FUN='sum')
+
+# Find average number of calls and add a category for Below Average and Above Average
+avg_calls <- apps_primary_days %>% select(CallsOffered) %>% sum()
+avg_calls <- avg_calls/nrow(apps_primary_days)
+apps_primary_days <- apps_primary_days %>% mutate(Volume=ifelse(CallsOffered < avg_calls, 1, 0))
+apps_primary_days$Volume <- apps_primary_days$Volume %>% as.factor()
+
+# Fit the model
+train <- apps_primary_days[1:132,]
+test <- apps_primary_days[133:176,]
+
+model <- glm(Volume ~ mday(Date) + month(Date) + weekdays(Date), family=binomial, data=train)
+summary(model)
+anova(model, test="Chisq")
+
+# Assessing Predictability
+fitted <- predict(model, newdata=test, type='response')
+fitted <- ifelse(fitted > 0.5, 0, 1)
+misClasificError <- mean(fitted != test$Volume)
+print(paste('Accuracy', 1-misClasificError))
+
+library(ROCR)
+p <- predict(model, newdata=test, type='response')
+pr <- prediction(p, test$Volume)
+prf <- performance(pr, measure="tpr", x.measure="fpr")
+plot(prf)
+auc <- performance(pr, measure="auc")
+auc <- auc@y.values[[1]]
+auc
