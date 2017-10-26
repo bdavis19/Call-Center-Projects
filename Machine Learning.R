f476dd53d783c9ae9c5a4cef2e1869b7ad583004
Barry Davis <bdavis19@gmail.com>
2017-10-26 00:18:08 -0400

    Reworked some of the logistic regression and added random forest for comparison.

--- Machine Learning.R
+++ Machine Learning.R
@@ -5,45 +5,75 @@
 #########################################################################################################
 library(data.table)
 library(tidyverse)
+library(lubridate)
+
 # Load Data
-apps_primary <- fread("apps_primary_refined.csv")
+primaryApps <- fread("apps_primary_refined.csv")
 
 # Check missing values
-sapply(apps_primary, function(x) sum(is.na(x)))
-sapply(apps_primary, function(x) length(unique(x)))
+sapply(primaryApps, function(x) sum(is.na(x)))
+sapply(primaryApps, function(x) length(unique(x)))
 library(Amelia)
-missmap(apps_primary, main="Missing values vs observed")
+missmap(primaryApps, main="Missing values vs observed")
 
-# Get data into df by day
-apps_primary_days <- apps_primary
-apps_primary_days <- apps_primary_days %>% mutate(Date=as.Date(statTimestamp), statTimestamp = NULL, ApplicationID=NULL, ApplicationName=NULL)
-apps_primary_days <- aggregate(. ~ Date, apps_primary_days, FUN='sum')
+# Get date into some workable formats
+primaryApps$statTimestamp <- ymd_hms(primaryApps$statTimestamp)
+primaryApps <- primaryApps %>% mutate(Date = as.Date(statTimestamp), statTimestamp = NULL, ApplicationName = NULL, ApplicationID = NULL)
+primaryApps <- aggregate(. ~ Date, primaryApps, FUN='sum')
 
-# Find average number of calls and add a category for Below Average and Above Average
-avg_calls <- apps_primary_days %>% select(CallsOffered) %>% sum()
-avg_calls <- avg_calls/nrow(apps_primary_days)
-apps_primary_days <- apps_primary_days %>% mutate(Volume=ifelse(CallsOffered < avg_calls, 1, 0))
-apps_primary_days$Volume <- apps_primary_days$Volume %>% as.factor()
+# Average calls per day
+avgCallsPerDay <- sum(primaryApps$CallsOffered) / length(unique(paste(month(primaryApps$Date), day(primaryApps$Date), sep = "/")))
+
+# Create new df so that each observation is a daily total
+primaryApps <- primaryApps %>% group_by(Date) %>% summarise(Sum = sum(CallsOffered))
+
+# Add classification variable
+primaryApps <- primaryApps %>% mutate(Volume = ifelse(Sum > avgCallsPerDay, 1, 0))
 
 # Fit the model
-train <- apps_primary_days[1:132,]
-test <- apps_primary_days[133:176,]
-
-model <- glm(Volume ~ mday(Date) + month(Date) + weekdays(Date), family=binomial, data=train)
-summary(model)
-anova(model, test="Chisq")
+set.seed(1979)
+split <- sample.split(primaryApps$Volume, SplitRatio = 0.7)
+train <- subset(primaryApps, split == TRUE)
+test <- subset(primaryApps, split == FALSE)
+primaryApps.mod <- glm(Volume ~ mday(Date) + month(Date) + weekdays(Date), family=binomial, data=train)
+summary(primaryApps.mod)
+anova(primaryApps.mod, test="Chisq")
 
 # Assessing Predictability
-fitted <- predict(model, newdata=train, type='response')
-fitted <- ifelse(fitted > 0.5, 1, 0)
-misClasificError <- mean(fitted != train$Volume)
+primaryApps.pred <- predict(primaryApps.mod, newdata=test, type='response')
+primaryApps.pred <- ifelse(primaryApps.pred > 0.5, 1, 0)
+misClasificError <- mean(primaryApps.pred != test$Volume)
 print(paste('Accuracy', 1-misClasificError))
 
 library(ROCR)
-p <- predict(model, newdata=train, type='response')
-pr <- prediction(p, train$Volume)
+p <- predict(primaryApps.mod, newdata=test, type='response')
+pr <- prediction(p, test$Volume)
 prf <- performance(pr, measure="tpr", x.measure="fpr")
 plot(prf)
 auc <- performance(pr, measure="auc")
 auc <- auc@y.values[[1]]
 auc
+primaryApps.sse <- sum((primaryApps.pred - test$Volume)^2)
+primaryApps.sse
+table(test$Volume, primaryApps.pred)
+
+###############################################################################################
+# Tree Regression
+###############################################################################################
+library(caTools)
+library(rpart)
+library(rpart.plot)
+
+set.seed(1979)
+
+# Get the data into test and training sets
+split <- sample.split(primaryApps$Volume, SplitRatio = 0.7)
+primaryApps.train <- subset(primaryApps, split == TRUE)
+primaryApps.test <- subset(primaryApps, split == FALSE)
+primaryApps.tree <- rpart(Volume ~ month(Date) + mday(Date) + weekdays(Date), data = primaryApps.train)
+prp(primaryApps.tree)
+primaryApps.tree.pred <- predict(primaryApps.tree, newdata = primaryApps.test)
+primaryApps.tree.sse <- sum((primaryApps.tree.pred - primaryApps.test$Volume)^2)
+primaryApps.tree.sse
+
+# Random forest appears to be a better model in this case.
